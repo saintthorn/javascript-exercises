6770d7e610c41d9c40979b3b4b2beb194496fcf6
Spencer Thornhill <spencerthornhill@icloud.com>
2026-04-08 17:35:32 -0700

    solve 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  result = "";
+  let length = str.length;
+  for (let i = 0; i <= length - 1; i++) {
+    str.trim();
+    result = result + str.slice(-1);
+    str = str.slice(0, -1);
+  }
+  return result;
 };
+
+reverseString("Hello World!");
 
 // Do not edit below this line
 module.exports = reverseString;
