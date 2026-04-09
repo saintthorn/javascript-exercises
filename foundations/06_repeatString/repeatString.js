65ddee73b8eec5d25fdf28b82aff8c19c0e4af90
Spencer Thornhill <spencerthornhill@icloud.com>
2026-04-08 17:16:57 -0700

    solve 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (str, num) {
+  if (num < 0) {
+    return `ERROR`;
+  }
 
+  let result = "";
+
+  for (let i = 1; i <= num; i++) {
+    result = result + str;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
