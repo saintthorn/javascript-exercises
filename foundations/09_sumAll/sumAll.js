1282650b2a26c736ff19526b0ee80e2aa5a19fbc
Spencer Thornhill <spencerthornhill@icloud.com>
2026-04-10 11:26:26 -0700

    solve 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+  if (x > y) {
+    let temp = x;
+    x = y;
+    y = temp;
+  }
 
+  if (x < 0 || y < 0) {
+    return "ERROR";
+  } else if (typeof x !== "number" || typeof y !== "number") {
+    return "ERROR";
+  } else if (!Number.isInteger(x) || !Number.isInteger(y)) {
+    return "ERROR";
+  }
+
+  let sum = 0;
+  for (let i = x; i <= y; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
