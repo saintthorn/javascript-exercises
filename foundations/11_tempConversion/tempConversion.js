4c5b0ad5997f17725cfc4095c09db92a9473dc9d
Spencer Thornhill <spencerthornhill@icloud.com>
2026-04-10 11:55:26 -0700

    solve 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fah) {
+  let result = (fah - 32) / (9 / 5);
+  if (result === 0) {
+    return result;
+  }
+  result = Number(result.toFixed(1));
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  let result = cel * (9 / 5) + 32;
+  if (result === 0) {
+    return result;
+  }
+  result = Number(result.toFixed(1));
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
