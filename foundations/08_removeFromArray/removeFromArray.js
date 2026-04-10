b9b9a9db5bccfc6fa88ce71162466240648a2c26
Spencer Thornhill <spencerthornhill@icloud.com>
2026-04-10 11:14:03 -0700

    solve 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, opt1, opt2, opt3, opt4) {
+  const optional = [opt1, opt2, opt3, opt4];
+
+  return arr.filter((item) => !optional.includes(item));
 };
 
 // Do not edit below this line
